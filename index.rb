9b79c835427b265fe1a48f774f6adf891a5f8130
janice <>
2020-05-05 01:20:47 +0800

    modify api path

--- index.rb
+++ index.rb
@@ -6,11 +6,7 @@ require 'dotenv'
 enable :sessions
 set :session_secret, 'super secret'
 
-before do
-    Dotenv.load('.env')
-    @path  = ENV['APIPATH']
-end
-
+path = 'https://courseplatform-rails.herokuapp.com/api/v1'
 get '/' do
     if session[:access_token]
         redirect '/courses'
@@ -20,14 +16,14 @@ get '/' do
 end
 
 get '/courses' do
-    api = "#{@path}/original/courses/"
+    api = "#{path}/original/courses/"
     response = Faraday.get(api, nil ,{'access_token' => session[:access_token]})
     @courses = JSON.parse(response.body)
     erb :home, {:layout => :index_layout}
 end
 
 get '/course/:id' do
-    api = "#{@path}/original/courses/#{params[:id]}"
+    api = "#{path}/original/courses/#{params[:id]}"
     response = Faraday.get(api,nil,{'access_token' => session[:access_token]})
     @course = JSON.parse(response.body)["course"]
     @bought = JSON.parse(response.body)["bought"]
@@ -35,7 +31,7 @@ get '/course/:id' do
 end
 
 get '/course/:id/buy' do
-    api = "#{@path}/original/courses/#{params[:id]}"
+    api = "#{path}/original/courses/#{params[:id]}"
     response = Faraday.post(api,nil,{'access_token' => session[:access_token]})
     response_body = JSON.parse(response.body)
     binding.pry
@@ -48,21 +44,21 @@ get '/course/:id/buy' do
 end
 
 get '/records' do
-    api = "#{@path}/orderrecord/"
+    api = "#{path}/orderrecord/"
     response = Faraday.get(api,nil,{'access_token' => session[:access_token]})
     @records = JSON.parse(response.body)
     erb :records, {:layout => :index_layout}
 end
 
 get '/record/:course_id' do
-    api = "#{@path}/orderrecord/#{params[:course_id]}"
+    api = "#{path}/orderrecord/#{params[:course_id]}"
     response = Faraday.get(api,nil,{'access_token' => session[:access_token]})
     @record = JSON.parse(response.body)
     erb :single_record, {:layout => :index_layout}
 end
 
 get '/filter/course_type' do
-    api = "#{@path}/orderrecord/filter/course_type/?type=#{params[:course_type]}"
+    api = "#{path}/orderrecord/filter/course_type/?type=#{params[:course_type]}"
     response = Faraday.get(api,nil,{'access_token' => session[:access_token]})
     @records = JSON.parse(response.body)
     puts "#{@records}"
@@ -70,7 +66,7 @@ get '/filter/course_type' do
 end
 
 get '/filter/unexpired' do
-    api = "#{@path}/orderrecord/filter/unexpired/"
+    api = "#{path}/orderrecord/filter/unexpired/"
     response = Faraday.get(api,nil,{'access_token' => session[:access_token]})
     @records = JSON.parse(response.body)
     erb :records, {:layout => :index_layout}
@@ -80,7 +76,7 @@ end
 
 
 post '/login' do
-    api = "#{@path}/sessions/?email=#{params[:email]}&password=#{params[:password]}"
+    api = "#{path}/sessions/?email=#{params[:email]}&password=#{params[:password]}"
     response = Faraday.post(api)
     response_body = JSON.parse(response.body)
     if response_body["error_code"]
@@ -96,7 +92,7 @@ get '/signup' do
     erb :signup, {:layout => :index_layout}
 end
 post '/signup' do
-    api = "#{@path}/signup/?user[email]=#{params[:email]}&user[password]=#{params[:password]}&user[password_confirmation]=#{params[:password_confirmation]}"
+    api = "#{path}/signup/?user[email]=#{params[:email]}&user[password]=#{params[:password]}&user[password_confirmation]=#{params[:password_confirmation]}"
     response = Faraday.post(api)
     response_body = JSON.parse[response.body]
     if response_body["error_code"]
